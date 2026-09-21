03a3dfd6614c9285e70875ffe996ebed732304b4
Monalisha Rakshit <rakshitmonalisha87@gmail.com>
2026-09-21 23:29:18 +0530

    Update test.py

--- document loaders/test.py
+++ document loaders/test.py
@@ -1,13 +1,70 @@
+# from langchain_community.document_loaders import TextLoader
+
+# data = TextLoader("document loaders/notes.txt",
+#                       encoding="utf-8"
+#                   )
+
+# docs = data.load()
+
+# print(docs[0].page_content)
+
+
+#print(len(docs))
+# docs (document) contains 2 things -> 
+# 1. matadata , 2. Page_content
+
+
+
+
+
+##### Character Based Splitting
+
 from langchain_community.document_loaders import TextLoader
 
-data = TextLoader("document loaders/notes.txt",
+from langchain_text_splitters import CharacterTextSplitter
+
+splitter = CharacterTextSplitter(
+    separator= "",
+    chunk_size = 10,
+    chunk_overlap = 1
+)
+
+data = TextLoader("document loaders/testt.txt",
                       encoding="utf-8"
                   )
 
 docs = data.load()
 
-print(docs[0].page_content)
-#print(len(docs))
+chunks = splitter.split_documents(docs)
 
-# docs (document) contains 2 things -> 
-# 1. matadata , 2. Page_content
+print(len(chunks))
+
+for i in chunks:
+    print(i.page_content)
+    print()
+    print()
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
