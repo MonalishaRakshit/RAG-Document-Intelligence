74985b1655559678244cc556a7c81354f5d0e15a
Monalisha Rakshit <rakshitmonalisha87@gmail.com>
2026-09-25 23:15:16 +0530

    modify chunk creation code

--- main.py
+++ main.py
@@ -70,15 +70,53 @@
 
 
 ########## here we feed our LLM Model external data source (extract data from PDF document)
+# from dotenv import load_dotenv
+# from langchain_groq import ChatGroq
+# from langchain_community.document_loaders import PyPDFLoader
+# from langchain_core.prompts import ChatPromptTemplate
+
+# load_dotenv()
+
+# data = PyPDFLoader("document loaders/GRU.pdf")  #load data
+# docs = data.load()  # convert data into document object
+
+# template = ChatPromptTemplate.from_messages(
+#     [("system","you are a AI that summarizes the text"),
+#      ("human", "{data}")
+#      ]
+# )
+
+# model = ChatGroq(
+#     model="openai/gpt-oss-20b"
+# )
+
+# prompt = template.format_messages(data = docs[0].page_content)
+
+# result = model.invoke(prompt)
+
+# print(result.content)
+
+
+
+
+ #### splitting very large document by creating chunks
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
 data = PyPDFLoader("document loaders/GRU.pdf")  #load data
 docs = data.load()  # convert data into document object
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 700,
+    chunk_overlap = 200
+)
+
+chunks = splitter.split_documents(docs)
 
 template = ChatPromptTemplate.from_messages(
     [("system","you are a AI that summarizes the text"),
@@ -95,5 +133,7 @@ prompt = template.format_messages(data = docs[0].page_content)
 result = model.invoke(prompt)
 
 print(result.content)
+
+
 
 
